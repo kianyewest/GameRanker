260309b44c4d74f2ee3e29d0d820412bb9839a08
kian <foulkekian@ecs.vuw.ac.nz>
2021-06-12 23:35:24 +1200

    add option to hide graph

--- src/SportHome.js
+++ src/SportHome.js
@@ -26,7 +26,7 @@ import {
 } from "./Functions";
 import { SPORTS_ENUM } from "./Sports";
 
-import { LineChart, Line } from 'recharts';
+import { LineChart, Line } from "recharts";
 
 const useStyles = makeStyles((theme) => ({
   formControl: {
@@ -48,7 +48,7 @@ function SportHome() {
   const [cancelToken, setCancelToken] = useState(axios.CancelToken.source());
   const [selectedDate, handleDateChange] = useState();
   const [events, setEvents] = useState({});
-  const [displayAllScores,setDisplayAllScores] = useState(false)
+  const [displayAllScores, setDisplayAllScores] = useState(false);
   const [displayScores, setDisplayScores] = useState([]);
   const [interestMargin, setInterestMargin] = useState({
     defaultHigh: 0.1,
@@ -138,7 +138,10 @@ function SportHome() {
             >
               {Object.keys(SPORTS_ENUM).map((sportID) => {
                 return (
-                  <MenuItem value={SPORTS_ENUM[sportID].id} key={SPORTS_ENUM[sportID].id}>
+                  <MenuItem
+                    value={SPORTS_ENUM[sportID].id}
+                    key={SPORTS_ENUM[sportID].id}
+                  >
                     {SPORTS_ENUM[sportID].id}
                   </MenuItem>
                 );
@@ -187,12 +190,14 @@ function SportHome() {
           />
         </Grid>
         <Grid item xs={12} lg={4}>
-        <Switch
-        checked={displayAllScores}
-        onChange={(event)=>{setDisplayAllScores(event.target.checked)}}
-        name="checkedA"
-        inputProps={{ 'aria-label': 'secondary checkbox' }}
-      />
+          <Switch
+            checked={displayAllScores}
+            onChange={(event) => {
+              setDisplayAllScores(event.target.checked);
+            }}
+            name="checkedA"
+            inputProps={{ "aria-label": "secondary checkbox" }}
+          />
         </Grid>
       </Grid>
 
@@ -215,16 +220,14 @@ function SportHome() {
           />
         </Grid>
       )}
-     
     </Grid>
   );
 }
 
 const DisplayInterestSlider = ({ date, interestMargin, setInterestMargin }) => {
   const max = 0.5;
-  const min=0.001;
-  
-  
+  const min = 0.001;
+
   return (
     <div style={{ paddingRight: "50px" }}>
       <Typography id="continuous-slider" gutterBottom>
@@ -251,7 +254,7 @@ const DisplayEvents = ({
   setDisplayScores,
 
   activeSport,
-  displayAllScores
+  displayAllScores,
 }) => {
   return (
     <Grid container item xs={12}>
@@ -279,9 +282,9 @@ const DisplayEvent = ({
   displayScores,
   setDisplayScores,
   activeSport,
-  displayAllScores
+  displayAllScores,
 }) => {
-  console.log("event: ",event)
+  console.log("event: ", event);
   return (
     <Paper elevation={3} style={{ padding: "10px", margin: "10px" }}>
       <h3>{event.name}</h3>
@@ -299,10 +302,6 @@ const DisplayEvent = ({
           displayAllScores={displayAllScores}
         />
       )}
-
-   
-
-    
     </Paper>
   );
 };
@@ -313,34 +312,10 @@ const DisplayEventScores = ({
   displayScores,
   setDisplayScores,
   activeSport,
-  displayAllScores
+  displayAllScores,
 }) => {
-  const [graphVal,setGraphVal] = useState([])
-  const [graphValA,setGraphValA] = useState([])
-  if(data){
-  console.log("data",data)
-  const homeWinPercentage = data.winprobability.map((item)=>{return {uv:item.homeWinPercentage}})
-  console.log(homeWinPercentage)
-  
-  }
-  console.log(graphVal)
-  const graphData = [
-    {
-      name: 'Page A',
-      uv: 0,
+  const [displayGraph, setDisplayGraph] = useState(false);
 
-    },
-    {
-      name: 'Page B',
-      uv: 2,
-     
-    },
-    {
-      name: 'Page C',
-      uv: 0,
-      
-    }
-  ];
   return data ? (
     <Grid>
       <Grid item xs={6}>
@@ -353,37 +328,44 @@ const DisplayEventScores = ({
         </p>
       </Grid>
       <Grid item xs={6}>
-        {(!displayScores.includes(id) && !displayAllScores)? (
+      <button
+            onClick={() => {
+              setDisplayGraph((prev) => !prev);
+            }}
+          >
+            Display Graph
+          </button>
+      </Grid>
+      <Grid item xs={6}>
+        {!displayScores.includes(id) && !displayAllScores ? (
           <button
             onClick={() => {
               setDisplayScores((prev) => [...prev, id]);
             }}
           >
-            Display Scores
+            Display Score
           </button>
         ) : (
           getScore(data, activeSport).awayScore +
           " - " +
           getScore(data, activeSport).homeScore
         )}
-        <p>{graphVal.length>0 && graphVal[1]}</p>
-
-        <p>{graphValA.length>0 && graphValA[1]}</p>
-        <LineChart width={400} height={400} data={data.winprobability.map((item)=>{return {team:item.homeWinPercentage,zero:0.5}})}>
-    <Line type="monotone" dataKey="team" stroke="#8884d8" />
-    <Line type="monotone" dataKey="zero" stroke="#8884d8" />
-  </LineChart>
-        {/* {data && <LineGraph fillBelow={'rgba(200,67,23,0.1)'}
-  hover={true} gridX={true} data={[]} gridY={true} onHover={(v)=>{setGraphVal(v)}} accent={'palevioletred'}  data={data.winprobability.map(item=>0.5-item.homeWinPercentage)}/>}
-        {data && <LineGraph fillBelow={'rgba(200,67,23,0.1)'}
-  hover={true} gridX={true} gridY={true} onHover={(v)=>{setGraphValA(v)}} accent={'palevioletred'} data={data.winprobability.map(item=>item.homeWinPercentage)}/>} */}
+        {displayGraph &&<LineChart
+          style={{ overflow: "none" }}
+          width={400}
+          height={400}
+          data={data.winprobability.map((item) => {
+            return { team: item.homeWinPercentage, zero: 0.5 };
+          })}
+        >
+          <Line type="monotone" dataKey="team" stroke="#8884d8" />
+          <Line type="monotone" dataKey="zero" stroke="#8884d8" />
+        </LineChart>}
       </Grid>
     </Grid>
   ) : (
     "loading"
   );
 };
-
-
 
 export default SportHome;
