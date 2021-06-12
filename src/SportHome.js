05634bf8dffb54bf40661a35d024c592d379aa30
kian <foulkekian@ecs.vuw.ac.nz>
2021-06-12 16:28:04 +1200

    display message if no events on

--- src/SportHome.js
+++ src/SportHome.js
@@ -10,18 +10,28 @@ import { Container, Grid, Paper } from "@material-ui/core";
 import ArrowBackIosIcon from "@material-ui/icons/ArrowBackIos";
 import ArrowForwardIosIcon from "@material-ui/icons/ArrowForwardIos";
 import Slider from "@material-ui/core/Slider";
-import MenuItem from '@material-ui/core/MenuItem';
-import FormHelperText from '@material-ui/core/FormHelperText';
-import FormControl from '@material-ui/core/FormControl';
-import Select from '@material-ui/core/Select';
-import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from "@material-ui/core/MenuItem";
+import FormHelperText from "@material-ui/core/FormHelperText";
+import FormControl from "@material-ui/core/FormControl";
+import Select from "@material-ui/core/Select";
+import InputLabel from "@material-ui/core/InputLabel";
 
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles } from "@material-ui/core/styles";
 
-const nba = Object.freeze({id:'nba',link:'basketball/nba', scoresID:'plays',name:"nba"})
-const nfl = Object.freeze({id:'nfl',link:'football/nfl', scoresID:'scoringPlays',name:"nfl"})
+const nba = Object.freeze({
+  id: "nba",
+  link: "basketball/nba",
+  scoresID: "plays",
+  name: "nba",
+});
+const nfl = Object.freeze({
+  id: "nfl",
+  link: "football/nfl",
+  scoresID: "scoringPlays",
+  name: "nfl",
+});
 
-const SPORTS_ENUM = Object.freeze({[nba.id]:nba, [nfl.id]:nfl})
+const SPORTS_ENUM = Object.freeze({ [nba.id]: nba, [nfl.id]: nfl });
 
 const useStyles = makeStyles((theme) => ({
   formControl: {
@@ -32,7 +42,6 @@ const useStyles = makeStyles((theme) => ({
     marginTop: theme.spacing(2),
   },
 }));
-
 
 const calculateInterest = (winprobability, highMargin, mildMargin) => {
   const interest = {
@@ -62,12 +71,11 @@ const calculateInterest = (winprobability, highMargin, mildMargin) => {
   return interest;
 };
 
-
-const getScore = (eventData,sport)=>{
-  const playArray = eventData[sport.scoresID]
-  const recentPlay = playArray[playArray.length - 1]
-  return recentPlay
-}
+const getScore = (eventData, sport) => {
+  const playArray = eventData[sport.scoresID];
+  const recentPlay = playArray[playArray.length - 1];
+  return recentPlay;
+};
 
 function SportHome() {
   const classes = useStyles();
@@ -80,14 +88,14 @@ function SportHome() {
     high: 0.1,
     mild: 0.2,
   });
-  const [age, setAge] = React.useState('');
-  const [activeSport,setActiveSport] = useState(SPORTS_ENUM.nfl)
+  const [loadingEvents, setLoadingEvents] = useState(true);
+  const [activeSport, setActiveSport] = useState(SPORTS_ENUM.nfl);
   const SPORT_ESPN_LINK = activeSport.link;
   useEffect(() => {
     const queryString = window.location.search;
     console.log("params: ", queryString);
     const urlParams = new URLSearchParams(queryString);
-    
+
     const dateStr = urlParams.get("date");
     if (dateStr) {
       console.log("got date string: ", dateStr);
@@ -103,7 +111,7 @@ function SportHome() {
 
     const sportStr = urlParams.get("sport");
     if (sportStr) {
-      const sport = SPORTS_ENUM[sportStr]
+      const sport = SPORTS_ENUM[sportStr];
       if (sport) {
         setActiveSport(sport);
       } else {
@@ -112,14 +120,14 @@ function SportHome() {
     } else {
       setActiveSport(SPORTS_ENUM.nba);
     }
-
   }, []);
 
   useEffect(() => {
-
+    setLoadingEvents(true);
     setEvents({});
     if (!selectedDate || isNaN(selectedDate.getTime())) {
       cancelToken.cancel("user requested events");
+      setLoadingEvents(false);
       return;
     }
 
@@ -144,18 +152,18 @@ function SportHome() {
         }
       })
       .then(function (response) {
-        console.log("response: ",response)
         if (!response) {
           return;
         }
-        
+        setLoadingEvents(false);
+
         const localEvents = {};
         response.data.events.forEach(
           (event) => (localEvents[event.id] = event)
         );
 
         setEvents(localEvents);
-         
+
         response.data.events.forEach((event) => {
           axios
             .get(
@@ -188,7 +196,7 @@ function SportHome() {
             });
         });
       });
-  }, [selectedDate,activeSport]);
+  }, [selectedDate, activeSport]);
 
   useEffect(() => {
     Object.keys(events).forEach((key) => {
@@ -213,25 +221,26 @@ function SportHome() {
       style={{ minHeight: "100vh" }}
     >
       <Grid item xs={12}>
-      <FormControl className={classes.formControl}>
-        <InputLabel id="demo-simple-select-label">Sport</InputLabel>
-      <Select
-          labelId="demo-simple-select-label"
-          id="demo-simple-select"
-          value={activeSport.name}
-          renderValue={()=>activeSport.name}
-          onChange={(event)=>{setActiveSport(event.target.value)}}
-        >
-          {/* TODO make this dynamic */}
-          {/* {Object.keys(SPORTS_ENUM).map((v)=>{
+        <FormControl className={classes.formControl}>
+          <InputLabel id="demo-simple-select-label">Sport</InputLabel>
+          <Select
+            labelId="demo-simple-select-label"
+            id="demo-simple-select"
+            value={activeSport.name}
+            renderValue={() => activeSport.name}
+            onChange={(event) => {
+              setActiveSport(event.target.value);
+            }}
+          >
+            {/* TODO make this dynamic */}
+            {/* {Object.keys(SPORTS_ENUM).map((v)=>{
             console.log(v);
             return <MenuItem value={v.id}>huh{v.name}</MenuItem>
           })}
            */}
-           <MenuItem value={nba}>{nba.name}</MenuItem>
-           <MenuItem value={nfl}>{nfl.name}</MenuItem>
-          
-        </Select>
+            <MenuItem value={nba}>{nba.name}</MenuItem>
+            <MenuItem value={nfl}>{nfl.name}</MenuItem>
+          </Select>
         </FormControl>
       </Grid>
       <Grid item xs={12}>
@@ -265,32 +274,30 @@ function SportHome() {
           <ArrowForwardIosIcon />
         </button>
       </Grid>
-      {selectedDate && !isNaN(selectedDate.getTime()) && (
-      <Grid item xs={12}>
-        <DisplayEvents
-          events={events}
-          date={selectedDate}
-          displayScores={displayScores}
-          setDisplayScores={setDisplayScores}
-          interestMargin={interestMargin}
-          setInterestMargin={setInterestMargin}
-          activeSport={activeSport}
-        />
-      </Grid>
+      {loadingEvents ? (
+        "Loading.."
+      ) : (
+        <Grid item xs={12}>
+          {selectedDate && !isNaN(selectedDate.getTime()) && 
+            <DisplayDate
+              date={selectedDate}
+              interestMargin={interestMargin}
+              setInterestMargin={setInterestMargin}
+            />
+          }
+          <DisplayEvents
+            events={events}
+            displayScores={displayScores}
+            setDisplayScores={setDisplayScores}
+            activeSport={activeSport}
+          />
+        </Grid>
       )}
     </Grid>
   );
 }
 
-const DisplayEvents = ({
-  events,
-  date,
-  displayScores,
-  setDisplayScores,
-  interestMargin,
-  setInterestMargin,
-  activeSport
-}) => {
+const DisplayDate = ({ date, interestMargin, setInterestMargin }) => {
   return (
     <div>
       {<p>Results for: {moment(date).format("DD/MM/YYYY")} (NZ time)</p>}
@@ -306,6 +313,19 @@ const DisplayEvents = ({
           setInterestMargin((prev) => ({ ...prev, high: val }));
         }}
       />
+    </div>
+  );
+};
+const DisplayEvents = ({
+  events,
+
+  displayScores,
+  setDisplayScores,
+
+  activeSport,
+}) => {
+  return (
+    <div>
       {Object.keys(events).length > 0
         ? Object.keys(events).map((key) => {
             return (
@@ -318,12 +338,17 @@ const DisplayEvents = ({
               />
             );
           })
-        : "loading..."}
+        : "No events"}
     </div>
   );
 };
 
-const DisplayEvent = ({ event, displayScores, setDisplayScores ,activeSport}) => {
+const DisplayEvent = ({
+  event,
+  displayScores,
+  setDisplayScores,
+  activeSport,
+}) => {
   const now = new Date();
   return (
     <Paper elevation={3} style={{ padding: "10px", margin: "10px" }}>
@@ -345,7 +370,13 @@ const DisplayEvent = ({ event, displayScores, setDisplayScores ,activeSport}) =>
   );
 };
 
-const DisplayEventScores = ({ id, data, displayScores, setDisplayScores ,activeSport}) => {
+const DisplayEventScores = ({
+  id,
+  data,
+  displayScores,
+  setDisplayScores,
+  activeSport,
+}) => {
   return data ? (
     <Grid>
       <Grid item xs={6}>
@@ -367,9 +398,9 @@ const DisplayEventScores = ({ id, data, displayScores, setDisplayScores ,activeS
             Display Scores
           </button>
         ) : (
-          getScore(data,activeSport).awayScore +
+          getScore(data, activeSport).awayScore +
           " - " +
-          getScore(data,activeSport).homeScore
+          getScore(data, activeSport).homeScore
         )}
       </Grid>
     </Grid>
